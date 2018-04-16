f84c444b3fb99d8968f91103bd4f6e2b859e8782
Paul McKibbin <pmckibbin@gmail.com>
2018-04-16 17:10:47 +0100

    Make json parse using quirks mode.

--- lib/config_files/loaders/json.rb
+++ lib/config_files/loaders/json.rb
@@ -5,7 +5,7 @@ module ConfigFiles
     class Json
       class << self
         def call(file_name, object_class: ::Hash)
-          ::JSON.load(file_name, nil, {object_class: object_class})
+          ::JSON.load(file_name, nil, {object_class: object_class, quirks_mode: true})
         end
       end
     end
